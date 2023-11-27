e12f2a2996f3ff73aa76212c1044bbc78136238c
Lennart Slusny <lennart@slusny.de>
2023-11-27 16:07:43 +0100

    f

--- calculate_climatology.py
+++ calculate_climatology.py
@@ -3,14 +3,76 @@ import os
 import numpy as np
 import sys
 
-path = sys.argv[1] #"/mnt/ssd2/Master/S2S_on_SFNO/outputs/sfno/"
-file = sys.argv[2] #"20231108.grib"
+# path = sys.argv[1] #"/mnt/ssd2/Master/S2S_on_SFNO/outputs/sfno/"
+# file = sys.argv[2] #"20231108.grib"
 
-file_path = os.path.join(path,file)
-print('file_path: ', file_path)
+# file_path = os.path.join(path,file)
+# print('file_path: ', file_path)
 
-ds = xr.open_dataset(file_path)
+basePath = "/mnt/qb/goswami/data/era5"
+
+param_sfc = [
+    "10m_u_component_of_wind", #from 1979 available, + 1979_2021 combined
+    "10m_v_component_of_wind", #from 1979 available, + 1979_2021 combined
+    "2m_temperature",          #from 1959 available, + 1959_2021 combined
+    "surface_pressure", 
+    "msl", 
+    "total_column_water_vapour", 
+    "100u", 
+    "100v"
+]
+
+combined_years = {
+    "10m_u_component_of_wind": "10m_u_component_of_wind_sfc_1979_2021.nc"
+}
+
+param_level_pl = (["t", "u", "v", "z", "r"], [1000, 850, 500, 250, 50])
+
+assets_extra_dir = "0.1"
+
+ordering = [
+    "10u",
+    "10v",
+    "2t",
+    "sp",
+    "msl",
+    "t850",
+    "u1000",
+    "v1000",
+    "z1000",
+    "u850",
+    "v850",
+    "z850",
+    "u500",
+    "v500",
+    "z500",
+    "t500",
+    "z50",
+    "r500",
+    "r850",
+    "tcwv",
+    "100u",
+    "100v",
+    "u250",
+    "v250",
+    "z250",
+    "t250",
+]
+
+paths = []
+# for sfc in param_sfc:
+#     paths.append(os.path.join(basePath, 'single_pressure_level', sfc, "*.nc"))
+# ds = xr.open_mfdataset(paths, concat_dim="time", preprocess=partial_func) 
+
+sfc = "2m_temperature/2m_temperature_sfc_1959_2021.nc"
+path = os.path.join(basePath, 'single_pressure_level', sfc)
+ds = xr.open_dataset(path)
 print(ds.info())
+# ds_grouped = ds.groupby("time.month").mean(dim="time")
+
+
+# ds = xr.open_dataset()
+# print(ds.info())
 
 # ds_sfno.groupby("step.days").mean(dim="step")
 # ds.to_netcdf("/home/gkd965/Files/ERA5_climatology.nc")
--- compare_S2S.py
+++ compare_S2S.py
@@ -23,6 +23,34 @@ task = SequentialTransform(
     )
 )
 
+
+from torch.utils.data.datapipes.iter.sharding import SHARDING_PRIORITIES
+from torchdata.datapipes.iter import IterDataPipe
+
+
+class ForecastingIterDataPipe(IterDataPipe):
+    def __init__(self, dp, steps, rate, dim='time', crop_size=None, crops_per_sample=1, shuffle=False):
+        assert crops_per_sample >= 1
+        assert rate >= 1
+        assert steps >= 1
+
+        pipe = dp.xr_unroll_indices(dim=dim, shuffle=shuffle)
+        pipe = pipe.sharding_filter(SHARDING_PRIORITIES.MULTIPROCESSING)
+        pipe = pipe.xr_extract_timeseries(steps, rate, dim=dim)
+        if crop_size:
+            if crops_per_sample > 1:
+                pipe = pipe.repeat(crops_per_sample)
+            pipe = pipe.xr_random_crop(crop_size, wraps={'lon': True})
+            if crops_per_sample > 1:
+                pipe = pipe.shuffle(buffer_size=crops_per_sample * 4)
+        self.dp = pipe
+
+    def __iter__(self):
+        return iter(self.dp)
+
+
+ForecastingTask = ForecastingIterDataPipe.as_transform
+
 builder = AtmodataPipeBuilder(
         era5,
         task,
