d3d8c2742ad5d4e16dc8493557b3f64dafaa73a1
Lennart Slusny <lennart@slusny.de>
2024-01-23 00:38:32 +0100

    f

--- climatology.py
+++ climatology.py
@@ -37,7 +37,7 @@ from time import time
 # The combined year data set (e.g. 2m_temperature_sfc_1959_2021.nc) are daily means)
 # The yearly data is hourly data (e.g. 2m_temperature_2021.nc)
 
-years = list(range(1979,2020)) # 
+years = list(range(1979,2018)) # 
 variable = '2m_temperature'
 # variable = '10m_v_component_of_wind'
 basePath = "/mnt/qb/goswami/data/era5"
