769412272a394cc29ca3f449c491033bb4eccd2a
Slusny <lennart@slusny.de>
2024-01-29 21:50:28 +0100

    f

--- modelskill_iterativ.py
+++ modelskill_iterativ.py
@@ -34,28 +34,30 @@ mean_files = {
 }
 # only single pressure level
 mean_file = os.path.join(basePath,"climate",mean_files[variable])
-model_file_sfno = os.path.join(basePath,'outputs/sfno','leadtime_8760_startDate_201901010_createdOn_20240123T0337/*')
-model_file_fcn = os.path.join(basePath,'outputs/fourcastnet','leadtime_8760_startDate_201901010_createdOn_20240123T0408/*')
+model_file_sfno = os.path.join(basePath,'outputs/sfno','leadtime_8760_startDate_201901010_createdOn_20240123T0337/leadtime_8760_startDate_201901010_createdOn_20240123T0337_step_{}.nc')
+model_file_fcn = os.path.join(basePath,'outputs/fourcastnet','leadtime_8760_startDate_201901010_createdOn_20240123T0408/leadtime_8760_startDate_201901010_createdOn_20240123T0408_step_{}.nc')
 save_file = os.path.join(basePath,'')
 dataPath = os.path.join("/mnt/qb/goswami/data/era5","single_pressure_level",variable,variable+"_{}.nc")
 
-ds_sfno = xr.open_mfdataset(model_file_sfno)[dataset_var] # needs 4 min
-ds_fcn = xr.open_mfdataset(model_file_fcn)[dataset_var]    # needs 22 mins ,chunks={'step':1}
 
-min_step = np.min([ds_sfno.shape[ds_sfno.dims.index('step')],ds_fcn.shape[ds_fcn.dims.index('step')]])
+ds_ref_alltimes  = xr.open_dataset(mean_file,chunks={'time':1})#.to_array().squeeze()[:min_step*6:6]
+g_truth = xr.open_dataset(dataPath.format(year))#.to_array().squeeze()[:min_step*6:6]
 
-ds_ref  = xr.open_dataset(mean_file,chunks={'time':1}).to_array().squeeze()[:min_step*6:6]
-g_truth = xr.open_dataset(dataPath.format(year)).to_array().squeeze()[:min_step*6:6]
+end = 128
+for idx in range(end):
+    s = (idx+1)*6
+    ds_sfno = xr.open_dataset(model_file_sfno.format(s))[dataset_var] # needs 4 min
+    ds_fcn = xr.open_mfdataset(model_file_fcn.format(s))[dataset_var] 
+    ds_ref = ds_ref_alltimes.sel(time=idx).to_array().squeeze()
+    truth = g_truth.sel(time=np.datetime64(str(year)+'-01-01T00:00:00.000000000') + np.timedelta64(s, 'h')).to_array().squeeze()
 
-rmse_sfno = xs.rmse(ds_sfno,g_truth,dim=["latitude","longitude"])
-rmse_fcn  = xs.rmse(ds_fcn ,g_truth,dim=["latitude","longitude"])
-rmse_ref  = xs.rmse(ds_ref,g_truth,dim=["latitude","longitude"])
+    rmse_sfno = xs.rmse(ds_sfno,truth,dim=["latitude","longitude"])
+    rmse_fcn  = xs.rmse(ds_fcn ,truth,dim=["latitude","longitude"])
+    rmse_ref  = xs.rmse(ds_ref,truth,dim=["latitude","longitude"])
 
-skill_score_sfno = 1 - rmse_sfno/rmse_ref
-skill_score_fcn  = 1 - rmse_fcn/rmse_ref
+    skill_score_sfno = 1 - rmse_sfno/rmse_ref
+    skill_score_fcn  = 1 - rmse_fcn/rmse_ref
 
-plt.figure()
-plt.plot(skill_score_sfno)
-plt.show()
-
-# skill_score_mean = xs.mean(dim=["latitude","longitude"]).to_pandas().to_numpy()
+    plt.figure()
+    plt.plot(skill_score_sfno)
+    plt.show()
