97280ae80c5804450b28fc75c181fdae45793da9
Slusny <lennart@slusny.de>
2024-01-29 08:43:43 +0100

    modelskill

--- modelskill.py
+++ modelskill.py
@@ -11,7 +11,9 @@ from time import sleep
 import psutil
 import gc
 from time import time
+import matplotlib.pyplot as plt
 
+year = 2019
 variable_index = 0
 variables = [
     '10m_u_component_of_wind',
@@ -29,17 +31,25 @@ mean_files = {
 
 }
 mean_file = os.path.join(basePath,"climate",mean_files[variable])
-model_file_sfno = os.path.join(basePath,'outputs/sfno','leadtime_8760_20231213-1809.grib')
-model_file_fcn = os.path.join(basePath,'outputs/fourcastnet','leadtime_8760_startDate_202101010_createdOn20231214-1204.grib')
+model_file_sfno = os.path.join(basePath,'outputs/sfno','leadtime_8760_startDate_201901010_createdOn_20240123T0337/*')
+model_file_fcn = os.path.join(basePath,'outputs/fourcastnet','leadtime_8760_startDate_201901010_createdOn_20240123T0408/*')
 save_file = os.path.join(basePath,'')
 dataPath = os.path.join("/mnt/qb/goswami/data/era5","single_pressure_level",variable,variable+"_{}.nc")
 
-ds_sfno = xr.open_dataset(model_file_sfno)[variable].to_array().squeeze()
-ds_fcn = xr.open_dataset(model_file_sfno)[variable].to_array().squeeze()
-ds_ref  = xr.open_dataset(mean_file).to_array().squeeze()[:ds_sfno.dims["step"]*6:6]
-g_truth = xr.open_dataset(dataPath.format(2021)).to_array().squeeze()[:ds_sfno.dims["step"]*6:6]
+ds_sfno = xr.open_mfdataset(model_file_sfno)[variable].to_array().squeeze()
+ds_fcn = xr.open_mfdataset(model_file_sfno)[variable].to_array().squeeze()
+
+min_step = np.min([ds_sfno.dims["step"],ds_fcn.dims["step"]])
+
+ds_ref  = xr.open_dataset(mean_file).to_array().squeeze()[:min_step*6:6]
+g_truth = xr.open_dataset(dataPath.format(year)).to_array().squeeze()[:min_step*6:6]
 
 
-skill_score_sfno = 1 - xs.rmse(ds_model,ds_ref,dim="time")/xs.rmse(g_truth,ds_ref,dim="time")
+skill_score_sfno = 1 - xs.rmse(ds_sfno,g_truth,dim=["latitude","longitude"])/xs.rmse(ds_ref,g_truth,dim=["latitude","longitude"])
+skill_score_fcn  = 1 - xs.rmse(ds_fcn ,g_truth,dim=["latitude","longitude"])/xs.rmse(ds_ref,g_truth,dim=["latitude","longitude"])
 
-skill_score_mean = skill_score.mean(dim=["latitude","longitude"]).to_pandas().to_numpy()
+plt.figure()
+plt.plot(skill_score_sfno)
+plt.show()
+
+# skill_score_mean = xs.mean(dim=["latitude","longitude"]).to_pandas().to_numpy()
