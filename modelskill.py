27127c909abe54b493e6250735d6af15b936a1c5
Slusny <lennart@slusny.de>
2024-01-29 14:44:15 +0100

    modellskill

--- modelskill.py
+++ modelskill.py
@@ -16,11 +16,13 @@ import matplotlib.pyplot as plt
 year = 2019
 variable_index = 0
 variables = [
-    '10m_u_component_of_wind',
-    '10m_v_component_of_wind',
-    '2m_temperature',
+    ('10m_u_component_of_wind', '10u'),
+    ('10m_v_component_of_wind','10v'),
+    ('2m_temperature','2t'),
+    ('total_column_water_vapour','tcwv')
 ]
-variable = variables[variable_index]
+variable = variables[variable_index][0]
+dataset_var = variables[variable_index][1]
 
 basePath = "/mnt/qb/work2/goswami0/gkd965/"
 mean_files = {
@@ -37,17 +39,20 @@ model_file_fcn = os.path.join(basePath,'outputs/fourcastnet','leadtime_8760_star
 save_file = os.path.join(basePath,'')
 dataPath = os.path.join("/mnt/qb/goswami/data/era5","single_pressure_level",variable,variable+"_{}.nc")
 
-ds_sfno = xr.open_mfdataset(model_file_sfno)[variable].to_array().squeeze()
-ds_fcn = xr.open_mfdataset(model_file_sfno)[variable].to_array().squeeze()
+ds_sfno = xr.open_mfdataset(model_file_sfno)[dataset_var]
+ds_fcn = xr.open_mfdataset(model_file_fcn)[dataset_var]    #,chunks={'step':1}
 
-min_step = np.min([ds_sfno.dims["step"],ds_fcn.dims["step"]])
+min_step = np.min([ds_sfno.shape[ds_sfno.dims.index('step')],ds_fcn.shape[ds_fcn.dims.index('step')]])
 
-ds_ref  = xr.open_dataset(mean_file).to_array().squeeze()[:min_step*6:6]
+ds_ref  = xr.open_dataset(mean_file,chunks={'time':1}).to_array().squeeze()[:min_step*6:6]
 g_truth = xr.open_dataset(dataPath.format(year)).to_array().squeeze()[:min_step*6:6]
 
+rmse_sfno = xs.rmse(ds_sfno,g_truth,dim=["latitude","longitude"])
+rmse_fcn  = xs.rmse(ds_fcn ,g_truth,dim=["latitude","longitude"])
+rmse_ref  = xs.rmse(ds_ref,g_truth,dim=["latitude","longitude"])
 
-skill_score_sfno = 1 - xs.rmse(ds_sfno,g_truth,dim=["latitude","longitude"])/xs.rmse(ds_ref,g_truth,dim=["latitude","longitude"])
-skill_score_fcn  = 1 - xs.rmse(ds_fcn ,g_truth,dim=["latitude","longitude"])/xs.rmse(ds_ref,g_truth,dim=["latitude","longitude"])
+skill_score_sfno = 1 - rmse_sfno/rmse_ref
+skill_score_fcn  = 1 - rmse_fcn/rmse_ref
 
 plt.figure()
 plt.plot(skill_score_sfno)
