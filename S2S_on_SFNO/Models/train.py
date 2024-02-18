5bb7169067d977953db614d11b15e2161c487d92
Slusny <lennart@slusny.de>
2024-02-18 15:39:48 +0100

    f

--- S2S_on_SFNO/Models/train.py
+++ S2S_on_SFNO/Models/train.py
@@ -44,8 +44,8 @@ class ERA5_galvani(IterableDataset):
         scf = sample[self.model.param_sfc_ERA5].to_array().to_numpy()
         pl = sample[list(self.model.levels_per_pl.keys())].sel(level=level_list).to_array().to_numpy()
         pl = pl.reshape((pl.shape[0]*pl.shape[1], pl.shape[2], pl.shape[3]))
-        u100 = self.dataset_u100.isel(time=self.start_idx + idx)["u100"].to_numpy()
-        v100 = self.dataset_v100.isel(time=self.start_idx + idx)["v100"].to_numpy()
+        u100 = self.dataset_u100.isel(time=self.start_idx + idx)["u100"].to_numpy()[None]
+        v100 = self.dataset_v100.isel(time=self.start_idx + idx)["v100"].to_numpy()[None]
         return torch.tensor(np.vstack((
             scf[:2],
             u100,
