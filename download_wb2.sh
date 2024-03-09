460ae7a94544978112ef4417e0e6d4373862793f
Lennart Slusny <lennart@slusny.de>
2024-03-09 16:03:14 +0100

    f

--- download_wb2.sh
+++ download_wb2.sh
@@ -1,7 +1,3 @@
 gsutil -m cp -r "gs://weatherbench2/datasets/era5/1959-2023_01_10-wb13-6h-1440x721_with_derived_variables.zarr" /mnt/qb/goswami/data/era5/weatherbench2/
 
-# gsutil -m cp -r \
-#   "gs://weatherbench2/datasets/era5/1959-2023_01_10-1h-240x121_equiangular_with_poles_conservative.zarr" \
-#   "gs://weatherbench2/datasets/era5-hourly-climatology/1990-2019_6h_1440x721.zarr" \
-#   "gs://weatherbench2/datasets/era5-hourly-climatology/1990-2019_6h_240x121_equiangular_with_poles_conservative.zarr" \
-#   "/mnt/qb/goswami/data/era5/weatherbench2/"
+gsutil -m cp -r gs://weatherbench2/datasets/era5/1959-2023_01_10-1h-240x121_equiangular_with_poles_conservative.zarr gs://weatherbench2/datasets/era5-hourly-climatology/1990-2019_6h_1440x721.zarr gs://weatherbench2/datasets/era5-hourly-climatology/1990-2019_6h_240x121_equiangular_with_poles_conservative.zarr /mnt/qb/goswami/data/era5/weatherbench2/
