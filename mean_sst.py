a03afc0adbce0710ec338f30429a96e31491ae51
Slusny <lennart@slusny.de>
2024-04-03 22:55:11 +0200

    f

---
+++ mean_sst.py
@@ -0,0 +1,17 @@
+import xarray as xr
+import os
+import numpy as np
+
+ds = xr.open_dataset("/mnt/qb/goswami/data/era5/weatherbench2/1959-2023_01_10-wb13-6h-1440x721_with_derived_variables.zarr")
+sst = ds["sea_surface_temperature"]
+
+save_path = "/mnt/qb/work2/goswami0/gkd965/Assets/sfno"
+
+mean = sst.mean()
+print("mean sst: ", mean)
+np.save(os.path.join(save_path,"global_means_sst.npy"), mean)
+std = sst.std()
+print("std sst: ", std)
+np.save(os.path.join(save_path,"global_std_sst.npy"), std)
+print("done")
+
