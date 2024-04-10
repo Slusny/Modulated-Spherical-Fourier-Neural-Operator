6f77c68facfae7a19643166c210144550c6f2b4d
Slusny <lennart@slusny.de>
2024-04-10 14:53:17 +0200

    cl

--- rechunk.py
+++ rechunk.py
@@ -5,9 +5,11 @@ from rechunker import rechunk
 from dask.diagnostics import ProgressBar
 import os
 
-uv = "u"
+uv = "v"
+print(uv)
 
 base_path = "/mnt/qb/goswami/data/era5/u100m_v100m_721x1440"
+save_path = "/mnt/qb/goswami/data/era5/weatherbench2/"
 uv100=uv+"100m_1959-2022_721x1440_correct_chunk_new_mean_INTERPOLATE.zarr"
 
 uv100_new=uv+"100m_chunked_1959-2022_721x1440_correct_chunk_new_mean_INTERPOLATE.zarr"
@@ -21,11 +23,11 @@ target_chunks = {
     "latitude": None,
     "longitude": None,
 }
-max_mem = "300GB"
+max_mem = "390GB"
 
-target_store = os.path.join(base_path,uv100_new)
-temp_store = os.path.join(base_path,uv100_temp)
-source_array = ds[uv+"100"]
+target_store = os.path.join(save_path,uv100_new)
+temp_store = os.path.join(save_path,uv100_temp)
+source_array = ds
 array_plan = rechunk(
     source_array, target_chunks, max_mem, target_store, temp_store=temp_store
 )
@@ -36,3 +38,37 @@ result = array_plan.execute()
 
 with ProgressBar():
     array_plan.execute()
+
+
+# uv = "u"
+
+# base_path = "/mnt/qb/goswami/data/era5/weatherbench2/"
+# # base_path = "/mnt/qb/goswami/data/era5/u100m_v100m_721x1440"
+# uv100="u100_smallset_test.zarr"
+
+# uv100_new="u100_chunked_smallset_test.zarr"
+# uv100_temp = "u100_temp_smallset_test.zarr"
+
+# ds =  xr.open_zarr(os.path.join(base_path, uv100))
+
+# target_chunks = {
+#     uv+"100": {"time": 1, "latitude": 721, "longitude": 1440},
+#     "time": None,  # don't rechunk this array
+#     "latitude": None,
+#     "longitude": None,
+# }
+# max_mem = "100GB"
+
+# target_store = os.path.join(base_path,uv100_new)
+# temp_store = os.path.join(base_path,uv100_temp)
+# source_array = ds
+# array_plan = rechunk(
+#     source_array, target_chunks, max_mem, target_store, temp_store=temp_store
+# )
+
+# print(array_plan)
+# result = array_plan.execute()
+
+
+# with ProgressBar():
+#     array_plan.execute()
--- slurm_rechunk.sh
+++ slurm_rechunk.sh
@@ -15,15 +15,15 @@
 # the job can use and see 4 CPUs (from max 24).
 # needet task count -n, maybe there is a better way to specify cores
 
-#SBATCH --partition=cpu-galvani#a100-galvani#a100-galvani#cpu-galvani#2080-galvani#cpu-long#cpu-short #gpu-v100  #gpu-2080ti #cpu-long
+#SBATCH --partition=cpu-galvani#cpu-galvani#a100-galvani#a100-galvani#cpu-galvani#2080-galvani#cpu-long#cpu-short #gpu-v100  #gpu-2080ti #cpu-long
 
-#SBATCH --mem-per-cpu=31G # Per CPU -> Per Core
+#SBATCH --mem-per-cpu=40G # Per CPU -> Per Core
 # the job will need 12GB of memory equally distributed on 4 cpus.(251GB are available in total on one node)
 
-#SBATCH --gres=gpu:1
+##SBATCH --gres=gpu:1
 #the job can use and see 1 GPUs (4 GPUs are available in total on one node) use SBATCH --gres=gpu:1080ti:1 to explicitly demand a Geforce 1080 Ti GPU. Use SBATCH --gres=gpu:A4000:1 to explicitly demand a RTX A4000 GPU
 
-#SBATCH --time=02-01:00
+#SBATCH --time=00-01:00
 # the maximum time the scripts needs to run
 # "minutes:seconds", "hours:minutes:seconds", "days-hours","days-hours:minutes" and "days-hours:minutes:seconds"
 
@@ -39,7 +39,7 @@
 #SBATCH --mail-user=lennart.slusny@student.uni-tuebingen.de
 # your mail address
 
-singularity exec --nv --bind /mnt/qb/goswami/data/era5,/mnt/qb/work2/goswami0/gkd965 /mnt/qb/work2/goswami0/gkd965/sfno_packages8.sif /opt/conda/envs/model/bin/python /home/goswami/gkd965/MasterML/rechunk.py
+singularity exec --nv --bind /mnt/qb/goswami/data/era5,/mnt/qb/work2/goswami0/gkd965 /mnt/qb/work2/goswami0/gkd965/rechunk.sif /opt/conda/envs/model/bin/python /home/goswami/gkd965/MasterML/rechunk.py
 
 echo DONE!
 
