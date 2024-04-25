966597ea55b58c9e658de2bf26ddee4e70c4ea04
Lennart Slusny <lennart@slusny.de>
2024-04-25 19:01:28 +0200

    f

--- eval_scripts/slurm_eval_sfnoee.sh
+++ eval_scripts/slurm_eval_sfnoee.sh
@@ -39,5 +39,5 @@
 #SBATCH --mail-user=lennart.slusny@student.uni-tuebingen.de
 # your mail address
 
-singularity exec --nv --bind /mnt/qb/goswami/data/era5,/mnt/qb/work2/goswami0/gkd965 /mnt/qb/work2/goswami0/gkd965/sfno_packages8.sif /opt/conda/envs/model/bin/python /home/goswami/gkd965/MasterML/main.py --model sfno --model-version latest --validation-epochs 50 --training-workers 5 --batch-size 1 --multi-step-validation 5 --validation-step-skip 5 --eval-models-autoregressive --eval-checkpoint-path /mnt/qb/work2/goswami0/gkd965/checkpoints/sfno/emissary-enterprise-8 --checkpoint-num 5
+singularity exec --nv --bind /mnt/qb/goswami/data/era5,/mnt/qb/work2/goswami0/gkd965 /mnt/qb/work2/goswami0/gkd965/sfno_packages8.sif /opt/conda/envs/model/bin/python /home/goswami/gkd965/MasterML/main.py --model sfno --model-version latest --validation-epochs 50 --training-workers 5 --batch-size 1 --multi-step-validation 5 --validation-step-skip 5 --eval-models-autoregressive --eval-checkpoint-path /mnt/qb/work2/goswami0/gkd965/checkpoints/sfno/emissary-enterprise-8 --eval-checkpoint-num 5
 echo DONE!
