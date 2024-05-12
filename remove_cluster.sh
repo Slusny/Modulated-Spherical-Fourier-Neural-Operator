d0084ae74ba2a85b6d0eca5f619b17d420ba551e
Slusny <lennart@slusny.de>
2024-05-12 23:18:37 +0200

    remove_cluster switch from ls to find

--- remove_cluster.sh
+++ remove_cluster.sh
@@ -2,7 +2,7 @@
 
 # This script removes the files from the cluster recursively from all subdirectories of the given directory $1
 shopt -s globstar 
-for f in $(ls -d -a $1/** ); do
+for f in $(find $1 -type f); do
     if [ -f $f ]; then
         #rm $f
         echo "Removing file $f"
