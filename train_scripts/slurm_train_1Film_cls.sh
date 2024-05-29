381fd258ce59b418ce1d679b90e886815ff358dc
Slusny <lennart@slusny.de>
2024-05-29 04:28:08 +0200

    f

--- train_scripts/slurm_train_1Film_cls.sh
+++ train_scripts/slurm_train_1Film_cls.sh
@@ -3,7 +3,7 @@
 #a) Define slurm job parameters
 ####
 
-#SBATCH --job-name=2fddp #21gcn
+#SBATCH --job-name=1fddp #21gcn
 
 #resources:
 
@@ -27,10 +27,10 @@
 # the maximum time the scripts needs to run
 # "minutes:seconds", "hours:minutes:seconds", "days-hours","days-hours:minutes" and "days-hours:minutes:seconds"
 
-#SBATCH --error=/home/goswami/gkd965/jobs/job.2filmCLS.ddp.%J.err
+#SBATCH --error=/home/goswami/gkd965/jobs/job.1filmCLS.ddp.%J.err
 # write the error output to job.*jobID*.err
 
-#SBATCH --output=/home/goswami/gkd965/jobs/job.2filmCLS.ddp.%J.out
+#SBATCH --output=/home/goswami/gkd965/jobs/job.1filmCLS.ddp.%J.out
 # write the standard output to job.*jobID*.out
 
 #SBATCH --mail-type=ALL
