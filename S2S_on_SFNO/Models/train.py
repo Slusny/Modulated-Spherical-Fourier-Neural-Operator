2776fead6f84afce1b9d5592fae7e670c09e8ec2
Slusny <lennart@slusny.de>
2024-05-29 03:29:20 +0200

    f

--- S2S_on_SFNO/Models/train.py
+++ S2S_on_SFNO/Models/train.py
@@ -409,7 +409,7 @@ class Trainer():
         shuffle= not self.cfg.no_shuffle
         if self.cfg.ddp:
             self.training_loader = DataLoader(self.dataset,num_workers=self.cfg.training_workers, batch_size=self.cfg.batch_size,shuffle=False,pin_memory=True,sampler=DistributedSampler(self.dataset,shuffle=shuffle))
-            self.validation_loader = DataLoader(self.dataset_validation,num_workers=self.cfg.training_workers, batch_size=self.cfg.batch_size,shuffle=False,pin_memory=True,sampler=DistributedSampler(self.dataset_validation,shuffle=shuffle))
+            self.validation_loader = DataLoader(self.dataset_validation,num_workers=0, batch_size=self.cfg.batch_size,shuffle=False,pin_memory=True,sampler=DistributedSampler(self.dataset_validation,shuffle=shuffle))
 
         else:
             self.training_loader = DataLoader(self.dataset,shuffle=shuffle,num_workers=self.cfg.training_workers, batch_size=self.cfg.batch_size)
--- main.py
+++ main.py
@@ -1090,4 +1090,6 @@ Do Transformers need to have a square input
 - validation with ddp, mean over all_reduce and then mean over iterations -> mean of means is ok
 - ddp optimizer expects model.parameters() on ddp model but we return parameters from sub models. we could give ddp-model as parameter of the get_parameter function, but actually how it is handled right now should be fine?
 -  clone state dict from checkpoint and then delete that checkpoint to gain memory
+
+- seperate batch_size and workers for training and validation
 '''
